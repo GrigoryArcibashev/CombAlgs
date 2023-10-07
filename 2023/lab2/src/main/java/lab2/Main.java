917c7796a85d5d9bf3f601a9f8af5d132c6882ea
arcibashev71 <arcibashev71@yandex.ru>
2023-10-07 21:21:18 +0500

    lab2: java init2

--- 2023/lab2/src/main/java/lab2/Main.java
+++ 2023/lab2/src/main/java/lab2/Main.java
@@ -1,7 +1,21 @@
 package lab2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+//        String[] nums = reader.readLine().split(" ");
+//
+//        FileWriter writer = new FileWriter("src/main/resources/output.txt");
+//        writer.write(String.valueOf(result));
+//        writer.flush();
     }
+
+
+
 }
