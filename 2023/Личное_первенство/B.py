c340443043f80388323689ebd9950ceb9f6a3074
arcibashev71 <arcibashev71@yandex.ru>
2023-10-08 20:41:59 +0500

    del личку

--- 2023/Личное_первенство/B.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-numbers = input().split()
-mult = int(numbers[0])
-for i in range(1, N):
-    mult &= int(numbers[i])
-ones_count = format(mult, 'b').count('1')
-print(2 ** ones_count)
